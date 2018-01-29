f88c1aa8ab4cd0f0f03d6d2d1c930880cc091d55
Ky Wojciechowski <kywoj@umich.edu>
2018-01-29 17:16:36 -0500

    Added while loop

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,10 @@
-answer = input("What is your question?")
+answer = ""
+
+while answer != "quit":
+	answer = input("What is your question?")
+	if answer == "quit":
+		break
+	elif answer[-1] == "?":
+		pass
+	else:
+		print("I'm sorry, I can only answer questions.")
